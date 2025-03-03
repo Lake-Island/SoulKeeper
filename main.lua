412dcbb961f9a99cf13ae9b86bb271e3ce9c1dca
Lake-Island <jonathan@lakeisland.net>
2025-03-04 00:16:38 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
 local _, core = ...
 
 local frame = nil
+
 local function soulkeeper(cmd)
-  if frame == nil then
-    frame = core.create_settings_frame()
-  elseif frame:IsShown() then
-    frame:Hide()
-  else 
-    frame:Show()
-  end
+    if frame == nil then
+        frame = core.create_settings_frame()
+    elseif frame:IsShown() then
+        frame:Hide()
+    else
+        frame:Show()
+    end
 end
 
 SLASH_SOULKEEPER1 = "/soulkeeper"
 SlashCmdList["SOULKEEPER"] = soulkeeper
-
 SLASH_SK1 = "/sk"
 SlashCmdList["SK"] = soulkeeper
